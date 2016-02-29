76a974d0c8d51208665c6b3dd4ac2f1ef2bf83e0
michaelcheng0102 <michaelcheng0102@gmail.com>
2016-02-29 17:01:17 +0800

    First commit

---
+++ lab0/main.cu
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
+#include "SyncedMemory.h"
+
+#define CHECK {\
+	auto e = cudaDeviceSynchronize();\
+	if (e != cudaSuccess) {\
+		printf("At " __FILE__ ":%d, %s\n", __LINE__, cudaGetErrorString(e));\
+		abort();\
+	}\
+}
+
+__global__ void SomeTransform(char *input_gpu, int fsize) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	//	Turn the first 64 character with lower case alphabet into upper case.
+	//	e.g. a to A, ignore B(already upper case) and ,(punctuation)
+	if (input_gpu[idx] >= 97 && input_gpu[idx] <= 122) {
+        input_gpu[idx] = input_gpu[idx]-32;
+    }
+}
+
+int main(int argc, char **argv)
+{
+	// init, and check
+	if (argc != 2) {
+		printf("Usage %s <input text file>\n", argv[0]);
+		abort();
+	}
+	FILE *fp = fopen(argv[1], "r");
+	if (not fp) {
+		printf("Cannot open %s", argv[1]);
+		abort();
+	}
+	// get file size
+	fseek(fp, 0, SEEK_END);
+	size_t fsize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	// read files
+	MemoryBuffer<char> text(fsize+1);
+	auto text_smem = text.CreateSync(fsize);
+	CHECK;
+	fread(text_smem.get_cpu_wo(), 1, fsize, fp);
+	text_smem.get_cpu_wo()[fsize] = '\0';
+	fclose(fp);
+
+	// TODO: do your transform here
+	char *input_gpu = text_smem.get_gpu_rw();
+	// An example: transform the first 64 characters to '!'
+	// Don't transform over the tail
+	// And don't transform the line breaks
+	SomeTransform<<<2, 32>>>(input_gpu, fsize);
+
+	puts(text_smem.get_cpu_ro());
+	return 0;
+}
